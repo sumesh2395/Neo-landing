fb7783f5f38a57ff7a866c55475f9aaf28a7ca90
sumesh2395 <sumesh2395@gmail.com>
2026-06-17 00:04:10 +0530

    meta code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Syne } from "next/font/google";
 import FloatingWhatsApp from "@/components/ui/FloatingWhatsApp";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -29,7 +30,32 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${syne.variable} h-full antialiased dark`}
     >
+      <head>
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1040736032236597');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1040736032236597&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         <FloatingWhatsApp />
       </body>
